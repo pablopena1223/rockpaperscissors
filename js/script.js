bb9174256365241a11b17a49fa2fe8fda8ddcd10
Pablo Pena <lito23.pjp@gmail.com>
2018-11-20 23:23:27 +0000

    final changes

--- js/script.js
+++ js/script.js
@@ -10,26 +10,48 @@ var computerChoice = "";
 var winner = ""; 
 
 $("#shoot").click(function(){
-    
+   
 var randomNumber = Math.random();
 
      userChoice = $("#input").val();
     $("#userchoice").text(userChoice);
-
     
-if(randomNumber > 0 && randomNumber < .33) {
-        $("#text").html("rock"); 
-        computerChoice = "rock"
-    } else if(randomNumber > .33 && randomNumber < .66){
-        $("#text").html("paper");
-        computerChoice = "paper"
-    } else if(randomNumber > .66 && randomNumber < 1){
-        $("#text").html("sissors");
-        computerChoice = "sissors"
+    function getcomputerChoice(){
+    var computerChoice = ["paper","rock","scissors"];
+    var randomChoice = Math.floor(Math.random() * computerChoice.length);
+    return computerChoice[randomChoice];
     }
     
-    $("#computerChoice").text(computerChoice);
-      
+    var cc = getcomputerChoice()
+    $("#computerChoice").text(cc);
+    
+ 
+    if (userChoice === "paper" && cc === "rock"){
+        $('#result').text('User Wins')
+    }
+    if (userChoice === 'paper' && cc === 'paper'){
+        $('#result').text('Tie, try again')
+    }
+    if (userChoice === 'paper' && cc === 'scissors'){
+        $('#result').text('Computer Wins')
+    }
+    if (userChoice === 'scissors' && cc === 'rock'){
+        $('#result').text('Computer Wins')
+    }
+    if (userChoice === 'scissors' && cc === 'paper'){
+        $('#result').text('User Wins')
+    }
+    if (userChoice === 'scissors' && cc === 'scissors'){
+        $('#result').text('Tie, try again')
+    }
+    if (userChoice === 'rock' && cc ==='rock'){
+        $('#result').text('Tie, try again')
+    }
+    if (userChoice === 'rock' && cc ==='scissors'){
+        $('#result').text('User Wins')
+    }
+    if (userChoice === 'rock' && cc ==='paper'){
+        $('#result').text('Computer Wins')
+    }
 });
-// DOCUMENT READY FUNCTION BELOW
 
